2c9d63995ae00861a22afb051e274e5c27568cc9
denyev <denevt87@gmail.com>
2019-03-06 11:38:03 +0300

    :tada: Initial commit

---
+++ backup.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+
+BACKUP_NAME="profi_club"
+BACKUP_PATH="${HOME}/_Backups/${BACKUP_NAME}"
+CURRENT_DATE=$(date +"%F")
+COUNT=$(ls -1 ${BACKUP_PATH}/${BACKUP_NAME}_${CURRENT_DATE}* | wc -l )
+
+nice -n 19 ionice -c3 \
+tar -czvf ${BACKUP_PATH}/${BACKUP_NAME}_${CURRENT_DATE}_$((${COUNT} + 1)).tgz --exclude="node_modules" \
+ ../${BACKUP_NAME}
---
+++ deploy.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+
+nice -n 19 ionice -c3 rsync --progress --stats --bwlimit=1000 -e="ssh -p 22" -aAhHvX \
+${HOME}/projects/landings/publicis_media/build/* \
+denyev@vh10.timeweb.ru:~/landings/public_html/profi_club/build/
