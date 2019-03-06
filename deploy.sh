050340045fa6236d18875b14cf5fa362ba5356f4
denyev <denevt87@gmail.com>
2019-03-06 15:22:49 +0300

    Fixes the script of deploy.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 
 nice -n 19 ionice -c3 rsync --progress --stats --bwlimit=1000 -e="ssh -p 22" -aAhHvX \
-${HOME}/projects/landings/publicis_media/build/* \
+${HOME}/projects/landings/profi_club/build/* \
 denyev@vh10.timeweb.ru:~/landings/public_html/profi_club/build/
