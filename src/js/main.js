9add3705a7e22d6e71966eb7e129df5810ed627b
denyev <denevt87@gmail.com>
2019-03-12 22:18:43 +0300

    Adds the slider for the filters form.

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,28 @@
 import './vendor';
+
+try {
+	$(function () {
+		var slider_range = $('.filters__slider');
+		var slider_amount = $('.filters__amount');
+		var slider_currency = '$';
+
+		slider_range.slider({
+			range: true,
+			min: 0,
+			max: 12700,
+			values: [0, 7230],
+			slide: function (event, ui) {
+				slider_amount.val(slider_currency + ui.values[0] + ' - ' + slider_currency + ui.values[1]);
+			},
+			classes: {
+				'ui-slider': 'filters__slider',
+				'ui-slider-handle': 'filters__slider-handle',
+				'ui-slider-range': 'filters__slider-range'
+			},
+		});
+		slider_amount.val(slider_currency + slider_range.slider('values', 0) +
+			' - ' + slider_currency + slider_range.slider('values', 1));
+	});
+} catch(error) {
+	console.error(error);
+}
--- src/js/vendor.js
+++ src/js/vendor.js
@@ -1,6 +1,7 @@
 import '@babel/polyfill';
 import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
+import 'components-jqueryui';
 
 svg4everybody();
 
