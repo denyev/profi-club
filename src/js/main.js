f5ec072fca0bb31d3d1f94fd8483b0b169c11ffc
denyev <denevt87@gmail.com>
2019-03-13 17:19:23 +0300

    Adds the ability to hide the menu items.

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,62 @@
+'use strict';
+
 import './vendor';
 
+const tabletScreenWidth = window.matchMedia('(min-width: 992px)');
+
+/**
+ * Toggle navigation.
+ */
 try {
-	$(function () {
-		var slider_range = $('.filters__slider');
-		var slider_amount = $('.filters__amount');
-		var slider_currency = '$';
+	let navigationToggle = () => {
+		const navigation = document.querySelector('.header__navigation');
+		const toggle = document.querySelector('.header__toggle .toggle');
+		let navModifier = 'header__navigation--closed';
+		let toggleModifier = 'toggle--active';
+
+		if (tabletScreenWidth.matches) {
+			navigation.classList.remove(navModifier);
+			toggle.classList.remove(toggleModifier);
+		} else {
+			navigation.classList.add(navModifier);
+			toggle.addEventListener('click', () => {
+				if (navigation.classList.contains(navModifier)) {
+					navigation.classList.remove(navModifier);
+					toggle.classList.add(toggleModifier);
+				} else {
+					navigation.classList.add(navModifier);
+					toggle.classList.remove(toggleModifier);
+				}
+			});
+		}
+	};
+
+	window.onload = () => {
+		navigationToggle();
+	};
+
+	window.onresize = () => {
+		navigationToggle();
+	};
+} catch(error) {
+	console.error(error);
+}
+
+/**
+ * Slider range.
+ */
+try {
+	$(() => {
+		const slider_range = $('.filters__slider');
+		const slider_amount = $('.filters__amount');
+		let slider_currency = '$';
 
 		slider_range.slider({
 			range: true,
 			min: 0,
 			max: 12700,
 			values: [0, 7230],
-			slide: function (event, ui) {
+			slide: (event, ui) => {
 				slider_amount.val(slider_currency + ui.values[0] + ' - ' + slider_currency + ui.values[1]);
 			},
 			classes: {
